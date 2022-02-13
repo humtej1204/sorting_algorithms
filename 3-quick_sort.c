a3d7fe13233e732cb427ed9f399367c3e4c48f89
Humberto Tejada <humtej1204@gmail.com>
2022-02-13 14:17:18 -0500

    Correcting betty style

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,7 +10,7 @@
  */
 void quick_sort(int *array, size_t size)
 {
-    rec_func(array, 0, size, size);
+	rec_func(array, 0, size, size);
 }
 
 /**
@@ -24,16 +24,16 @@ void quick_sort(int *array, size_t size)
  */
 void rec_func(int *array, int start, int end, size_t size)
 {
-    int index = 0;
+	int index = 0;
 
-    if (end - start <= 1)
-        return;
-    index = lomuto_partition(array, start, end, size);
+	if (end - start <= 1)
+		return;
+	index = lomuto_partition(array, start, end, size);
 
-    if (index > start)
-        rec_func(array, start, index, size);
+	if (index > start)
+		rec_func(array, start, index, size);
 
-    rec_func(array, index + 1, end, size);
+	rec_func(array, index + 1, end, size);
 }
 
 /**
@@ -47,36 +47,36 @@ void rec_func(int *array, int start, int end, size_t size)
  */
 size_t lomuto_partition(int *array, int start, int end, size_t size)
 {
-    int i = start, j = 0, tmp = 0, index_part = 0, save = 0;
-    int pivote = array[end - 1], flag = 0;
+	int i = start, j = 0, tmp = 0, index_part = 0, save = 0;
+	int pivote = array[end - 1], flag = 0;
 
-    for (i = start; i < end; i++)
-    {
-        if (array[i] > pivote)
-        {
-            for (j = i + 1; j < end; j++)
-            {
-                if (j == end - 1)
-                {
-                    flag = -1;
-                    save = i;
-                }
-                if (array[j] <= pivote)
-                {
-                    tmp = array[j];
-                    array[j] = array[i];
-                    array[i] = tmp;
-                    break;
-                }
-            }
-            print_array(array, size);
-            if (flag == -1)
-                break;
-        }
-    }
-    if (flag == 0)
-        return (end - 1);
-    index_part = save;
-    i = start;
-    return (index_part);
+	for (i = start; i < end; i++)
+	{
+		if (array[i] > pivote)
+		{
+			for (j = i + 1; j < end; j++)
+			{
+				if (j == end - 1)
+				{
+					flag = -1;
+					save = i;
+				}
+				if (array[j] <= pivote)
+				{
+					tmp = array[j];
+					array[j] = array[i];
+					array[i] = tmp;
+					break;
+				}
+			}
+			print_array(array, size);
+			if (flag == -1)
+				break;
+		}
+	}
+	if (flag == 0)
+		return (end - 1);
+	index_part = save;
+	i = start;
+	return (index_part);
 }
