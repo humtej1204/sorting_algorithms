8933453c7c04cbf01e3139d05017c38076c54ce1
Humberto Tejada Espinoza <90359858+humtej1204@users.noreply.github.com>
2022-02-13 14:29:13 -0500

    Line 17: Correcting conditional of the bucle

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,7 +12,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
